a9e60daf44762d298b268f62f4a734199e399f30
Ridwan Roshid <24814708@stu.mmu.ac.uk>
2026-03-18 16:51:32 +0000

    Add final Function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 const prompts = require('prompts');
+var hashPatterns = []
+var possibleHashes = []
 
 function checkHashLength(text) {
     textLength = text.length
@@ -9,14 +11,20 @@ function checkHashLength(text) {
 	// - If 128 characters, it may be SHA-512
 
     if (textLength == 32) {
-        return "MD5 or NTLM"
+        hashPatterns.push("32 Characters long")
+        possibleHashes.push(["MD5", "NTLM"])
+        // return "MD5 or NTLM"
     } else if (textLength == 64){
+        hashPatterns.push("64 Characters long")
         return "SHA-256"
     } else if (textLength == 96){
+        hashPatterns.push("96 Characters long")
         return "SHA-384"
     } else if (textLength == 128){
+        hashPatterns.push("128 Characters long")
         return "SHA-512"
     } else {
+        hashPatterns.push("Variable character length")
         return "Other"
     }
 }
@@ -28,13 +36,23 @@ function checkHashCharacters(text) {
         const element = textArr[index];
 
         if (["%", "/", ".", "$"].includes(element)) {
+            hashPatterns.push("Contains '% / . $' ")
             return "bcrypt or argon2"
         } else {
+            hashPatterns.push("Contains alphanumeric characters only")
             return "other"
         }
-        
     }
+}
 
+function checkHashPrefix(text) {
+    console.log(text.substring(0, 7))
+
+    if (text.substring(0, 7)  == "$argon2") {
+        hashPatterns.push("contains prefix 'argon2' ")
+    } else {
+        hashPatterns.push("no unique prefixes found")
+    }
 }
 
 async function main() {
@@ -48,7 +66,10 @@ async function main() {
     console.log("Hash: " + result.value)
     console.log("Hash length: " + checkHashLength(result.value))
     console.log("Hash characters: " + checkHashCharacters(result.value))
+    console.log("Hash Prefixes: " + checkHashPrefix(result.value))
 
+    console.log(possibleHashes)
+    console.log(hashPatterns)
 }
 
 main();
